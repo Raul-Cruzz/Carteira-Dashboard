5f29bf64d4d14a7c5faaecb1426c4f8ea68871ed
Rgomes3333 <raulgc268@outlook.com>
2023-05-10 18:58:00 -0300

    Projeto-Carteira-Dashboard

---
+++ src/Components/Toggle/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import Switch, { ReactSwitchProps } from 'react-switch';
+
+
+export const Container = styled.div`
+
+    display: flex;
+    align-Items: center;
+
+`;
+
+export const ToggleLabel = styled.span`
+    
+    color: ${props => props.theme.colors.white}
+
+
+`;
+
+export const ToggleSelector = styled(Switch).attrs<ReactSwitchProps>(
+    ({theme}) => ({
+        onColor: theme.colors.info,
+        offColor: theme.colors.warning
+    }))<ReactSwitchProps>`
+        margin: 0 7px;
+    `;
---
+++ src/Pages/SingIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingIn: React.FC = () => {
+
+    return(
+
+        <h1>SingIn</h1>
+    )
+}
+
+export default SingIn
